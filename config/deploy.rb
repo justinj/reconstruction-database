a5523c228eddb57b691597e1cb513e6b1555d320
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-04 11:10:21 -0400

    another change

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,10 +18,10 @@ set :deploy_to, "/home/www/rcdb"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
