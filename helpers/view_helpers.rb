c7ada58b0d25bbe17486bd75acf60562b633a717
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-04 21:24:09 -0400

    fixed comment highlighting

--- helpers/view_helpers.rb
+++ helpers/view_helpers.rb
@@ -1,7 +1,7 @@
 helpers do
   def format_solution(solution)
     solution = solution.gsub("\n", "<br>")
-    solution.gsub(/(\/\/.*?<br>)/,
+    solution.gsub(/(\/\/.*?(<br>|$))/,
                   '<span class="comment">\1</span>')
   end
 end
