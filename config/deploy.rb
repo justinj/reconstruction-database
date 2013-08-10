3d897eebf7718b52252df89a436fbde89bb9d2a7
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-10 10:34:38 -0400

    remove comments from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,12 @@
 set :application, "rcdb"
 set :repository,  "git@github.com:justinj/reconstruction-database.git"
 
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
 
-role :web, "www"                          # Your HTTP server, Apache/etc
-role :app, "www"                          # This may be the same as your `Web` server
+role :web, "www"
+role :app, "www"
 
 set :deploy_to, "/home/www/rcdb"
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
