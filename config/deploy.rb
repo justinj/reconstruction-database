1b3447e8f3915cebcadbec3779b72cc9b3b48d9f
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-16 11:21:14 -0400

    put to file

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :update do
-    run "echo $SHELL"
+    run "echo $SHELL >> /home/www/asdf"
     run "cd #{current_path} && bundle install --deployment"
   end
   task :restart, :roles => :app, :except => { :no_release => true } do
