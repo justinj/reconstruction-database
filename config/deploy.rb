d64f59bdb984a152a55b2a4aea4c74aef64fe246
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-16 11:17:09 -0400

    move to single command

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,7 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :update do
-    run "cd #{current_path}"
-    run "bundle install --deployment"
+    run "cd #{current_path} && bundle install --deployment"
   end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
