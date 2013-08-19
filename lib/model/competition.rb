1d43e47ee8961fd30a2214e1e6d611c9a0efdac4
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-18 21:47:07 -0400

    Make the competition guess the url

--- lib/model/competition.rb
+++ lib/model/competition.rb
@@ -16,7 +16,13 @@ module ReconDatabase
     end
 
     def url
-     "https://www.worldcubeassociation.org/results/c.php?i=#{code}"
+     "https://www.worldcubeassociation.org/results/c.php?i=#{url_code}"
+    end
+
+    private
+
+    def url_code
+      code || name.gsub(" ","")
     end
 
     def link
--- spec/competition_spec.rb
+++ spec/competition_spec.rb
@@ -3,5 +3,9 @@ module ReconDatabase
     it "generates its url" do
       Competition.new(code: "WC2013").url.must_equal "https://www.worldcubeassociation.org/results/c.php?i=WC2013"
     end
+
+    it "guesses the competition code if there isn't one" do
+      Competition.new(name: "Zune Open 2012").url.must_equal "https://www.worldcubeassociation.org/results/c.php?i=ZuneOpen2012"
+    end
   end
 end
