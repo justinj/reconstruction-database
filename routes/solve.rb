a51ba7b689520772217d6862d47dd5257ed3b6c3
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-22 12:43:32 -0400

    started again with the brest submissions

--- routes/solve.rb
+++ routes/solve.rb
@@ -2,11 +2,17 @@ get "/solve" do
   redirect "/"
 end
 
+get "/solve/brest_submit" do
+  erb :solve_submission
+end
+
+post "/solve/brest_parse" do
+  result = ReconDatabase::BrestParser.new(params["post_content"]).solves
+  @result = result
+  erb :brest_parse
+end
+
 get "/solve/:id" do
   @solve = ReconDatabase::Solve.where(id: params[:id]).first
   erb :solve
-end
-
-get "/solve/submission" do
-  erb :edit_solve
 end
