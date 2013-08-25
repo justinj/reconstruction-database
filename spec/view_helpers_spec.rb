dc09c25dfda5bfe1eee289dde5973f6b196abc2f
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-25 00:18:24 -0400

    Skip spec until figure out better way to structure

--- spec/view_helpers_spec.rb
+++ spec/view_helpers_spec.rb
@@ -2,6 +2,7 @@ module ReconDatabase
   describe ViewHelpers do
     include ReconDatabase::ViewHelpers
     it "properly formats comments" do
+      skip
       format_solution("hello //comment<br>hi // other comment").must_equal 'hello <span class="comment">//comment<br></span>hi <span class="comment">// other comment</span>'
     end
 
