c5245015faaa04fe6db53fff93d4a96036c480ea
Justin Jaffray <justin.jaffray@gmail.com>
2013-08-27 16:02:55 -0400

    Add unicorn config

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,22 +8,19 @@ set :rvm_type, :system
 
 set :scm, :git
 
-role :web, "int"
-role :app, "int"
+role :web, "rcdb"
+role :app, "rcdb"
 
 task :prod do
-  set :deploy_to, "/home/www/rcdb"
+  set :deploy_to, "/home/rcdb"
 end
 
-task :stage do
-  set :deploy_to, "/u/apps/rcdb"
-end
+after 'deploy:restart', 'unicorn:reload'
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cd /home/rails/current && bundle install"
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "cd /home/rcdb/current && bundle install"
   end
 end
---
+++ config/unicorn.rb
@@ -0,0 +1,7 @@
+listen "127.0.0.1:8080"
+worker_processes 2
+user "rcdb"
+working_directory "/home/rcdb/current"
+pid "/home/unicorn/pids/unicorn.pid"
+stderr_path "/home/unicorn/log/unicorn.log"
+stdout_path "/home/unicorn/log/unicorn.log"
