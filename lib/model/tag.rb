2114799c8823213b4da3f0e88d4de54a720a6456
Justin Jaffray <justin.jaffray@gmail.com>
2013-09-04 11:42:22 -0400

    clean up tag

--- lib/model/tag.rb
+++ lib/model/tag.rb
@@ -9,19 +9,29 @@ module RCDB
       end
 
       def filter_solves(dataset, params)
-        tags = params["tags"].to_s.split(/\s+/).map do |tag_name|
-          first(name: tag_name)
-        end.compact.map(&:id)
-
-        solves_with_average_tags = DB[:averages_tags].join(:solves, average_id: :average_id).select(:id, :tag_id)
-
-        solves_tags = DB[:solves_tags].union(solves_with_average_tags)
-        solves_tags = solves_tags.where(tag_id: tags)
+        tags = extract_tags(params)
+        solves_tags = full_solves_tags_table(tags)
 
         tags.inject(dataset) do |dataset, tag|
           valid_solves = solves_tags.where(tag_id: tag).select(:solve_id)
           dataset.where(id: valid_solves)
         end
+      end
+
+      private
+
+      def extract_tags(query_params)
+        query_params["tags"].to_s.split(/\s+/).map do |tag_name|
+          first(name: tag_name)
+        end.compact.map(&:id)
+      end
+
+      # We need to combine the table with average tags and solve tags to get
+      # the true table for tags
+      def full_solves_tags_table(tags)
+        solves_with_average_tags = DB[:averages_tags].join(:solves, average_id: :average_id).select(:id, :tag_id)
+
+        solves_tags = DB[:solves_tags].union(solves_with_average_tags).where(tag_id: tags)
       end
     end
   end
