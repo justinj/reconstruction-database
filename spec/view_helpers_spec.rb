3881c50cbf8682ec0ed0ac233e56455cc55b5637
Justin Jaffray <justin.jaffray@gmail.com>
2013-09-04 12:21:41 -0400

    Test date formatting

--- spec/view_helpers_spec.rb
+++ spec/view_helpers_spec.rb
@@ -10,5 +10,10 @@ module RCDB
       garronize(solve)
         .must_equal "http://alg.garron.us/?alg=R-_U&ini=U-_R&cube=2x2x2"
     end
+
+    it "formats the date of a solve" do
+      date = Date.new(2010,8,10)
+      date_added(stub(date_added: date)).must_equal "Aug 10, 2010"
+    end
   end
 end
