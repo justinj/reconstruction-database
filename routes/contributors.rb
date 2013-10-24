d99a4f49f294a6003e5f64e6bf4c1d835ad85528
Justin Jaffray <justin.jaffray@gmail.com>
2013-10-23 21:08:59 -0400

    don't show blank reconstructors

--- routes/contributors.rb
+++ routes/contributors.rb
@@ -1,10 +1,10 @@
 get "/contributors" do
   reconstructors = RCDB::Solve
     .group_and_count(:reconstructor_id)
-    .reject { |entry| entry[:reconstructor_id].blank? }
     .map { |entry| {
       count: entry[:count],
       reconstructor: RCDB::Reconstructor.first(id: entry[:reconstructor_id]).name } }
+    .reject { |entry| entry[:reconstructor].blank? }
 
   erb :contributors, locals: { entries: reconstructors }
 end
