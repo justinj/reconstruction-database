101d91dbf32ab33c7e9b6b06dd7b3914de542563
Justin Jaffray <justinjaffray@khanacademy.org>
2014-02-23 22:28:54 -0800

    clean up garronizer a little

--- lib/garronizer.rb
+++ lib/garronizer.rb
@@ -2,21 +2,21 @@ module RCDB
   class Garronizer
     class << self
       def garronize(solve)
-        params = {
+        garron_url_from_params(
           "alg" => escape_chars(solve.canonical_solution),
           "setup" => escape_chars(solve.scramble),
-          "type" => animtype(solve),
+          "type" => animation_type(solve),
           "puzzle" => solve.puzzle.garronized_name,
           "title" => garron_title(solve),
-        }
-        garron_url_from_params(params)
+        )
       end
 
       private
 
       def garron_url_from_params(params)
-        url_params = params.reject { |k, v| v.blank? }
-        .map { |k, v| "#{k}=#{v}"}.join("&")
+        url_params = params
+          .reject { |k, v| v.blank? }
+          .map { |k, v| "#{k}=#{v}"}.join("&")
         "http://alg.cubing.net/?#{url_params}"
       end
 
@@ -24,9 +24,9 @@ module RCDB
         alg.tr("' ", "-_").gsub("\n","%0A")
       end
 
-      # if we don't have a scramble we need to set the 'animtype' to
+      # if we don't have a scramble we need to set the 'type' to
       # have it set the cube up by doing the inverse of the solution
-      def animtype(solve)
+      def animation_type(solve)
         solve.scramble.blank? ? "reconstruction-end-with-setup" : "reconstruction"
       end
 
