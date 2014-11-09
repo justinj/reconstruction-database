7ed38c35f689a1190d06a00d404c0502cdec6451
Justin Jaffray <justin.jaffray@gmail.com>
2014-11-09 14:58:19 -0500

    Add selectize.js

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -33,4 +33,43 @@ $(function(){
      $("#stats-input").val($("#stat-sections").sortable("serialize")); 
      return true;
   });
+
+  var renderItem = function(item, escape) {
+    return '<div>' +
+      (item.name ? "<span class='name'>" + escape(item.name) + "</span>" : "") +
+      '</div>';
+  };
+
+  var renderOption = function(item, escape) {
+    var label = item.name || item.email;
+    var caption = item.name ? item.email : null;
+    return "<div>" +
+      "<span class='select-result'>" + escape(label) + "</span>" +
+      (caption ? "<span class='caption'>" + escape(caption) + "</span>" : "") +
+      "</div>";
+  };
+
+  // annoyingly selectize doesn't seem to allow you to add things that aren't in the list
+  var vals = $("#search-box").val().split(",");
+  for (var i = 0; i < vals.length; i++) {
+    searchableOptions.push(vals[i]);
+  }
+
+  $("#search-box").selectize({
+      persist: false,
+      maxItems: null,
+      valueField: "name",
+      labelField: "name",
+      searchField: ["name"],
+      options: searchableOptions.map(function(name) {
+        return {name: name};
+      }),
+      render: {
+        item: renderItem,
+        option: renderOption
+      },
+      create: function(input) {
+        return {name: input};
+      }
+  });
 });
